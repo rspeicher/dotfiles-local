521e271ebe42827d658f24d17d5b220665b7050b
Robert Speicher <rspeicher@gmail.com>
2015-02-23 23:07:39 -0500

    dotjs/github: Remove infinite scroll

--- host-pride/js/github.com.js
+++ host-pride/js/github.com.js
@@ -21,57 +21,6 @@ $(document).on('click', 'a[title="Gemfile"]', function() {
 
 linkGems()
 
-// Infinite scroll on commit logs
-// ------------------------------------------------------------------------------
-
-
-// Should be called on a window's "scroll" event
-// Determines if the <tt>waypoint_id</tt> is within the current viewable area and calls
-// <tt>callback</tt> if it is.
-function paginationWaypoint(waypoint_id, next_id, callback) {
-  var distanceTop = $(waypoint_id).offset().top - $(window).height()
-
-  if ($(window).scrollTop() > distanceTop) {
-    var link = $(waypoint_id).find(next_id)
-    if(link.length > 0) {
-      callback(link.attr('href'))
-    }
-  }
-}
-
-// Loads a link and calls a callback on success
-//
-// FIXME: Called synchronously so that we don't load a page more than once; this was the lazy way out
-function paginationLoadPage(link, success) {
-  $.ajax(link, {
-    async: false,
-    cache: false,
-    dataType: 'html',
-    success: success
-  })
-}
-
-// Provides a default callback to use with paginationWaypoint
-function waypointCallback(link) {
-  paginationLoadPage(link, successCallback)
-}
-
-var content_id  = 'h3.commit-group-heading, ol.commit-group'
-var waypoint_id = 'div.pagination:last'
-var next_id     = 'span+a, a+a'
-
-function successCallback(d) {
-  $d = $($.parseHTML(d))
-  $d.find(content_id).insertBefore(waypoint_id)
-  $(waypoint_id).replaceWith($d.find(waypoint_id))
-}
-
-if ($(content_id).length > 0 && $(waypoint_id).length > 0) {
-  $(window).bind('scroll', function() {
-    paginationWaypoint(waypoint_id, next_id, waypointCallback)
-  })
-}
-
 // Easy activity log scanning
 // Keep track of your last seen activity log entry, and fade out everything after it
 // ------------------------------------------------------------------------------
